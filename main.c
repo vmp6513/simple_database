1c731f2c2c44ea07cc31f3eb940d7e372af27c00
vmp6513 <hengyuran@whu.edu.cn>
2022-01-11 22:38:56 +0800

    2022/1/11 commit

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-// Ch2: Compiler and Virtual Machine.
-/* FAQ: 1. describe the program logic;
+// Ch3: An In-Memory, Append-Only, Single-Table Database.
+/* FAQ: 1.
  */
 
 typedef enum {
@@ -12,12 +13,57 @@ typedef enum {
     META_COMMAND_UNRECOGNIZED_COMMAND
 } MetaCommandResult;
 
-typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
 
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+void serialize_row(Row* source, void* dest) {
+    memcpy(dest + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(dest + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    memcpy(dest + EMAIL_SIZE, source->email, EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* dest) {
+    memcpy((void*)(dest->id), source + ID_OFFSET, ID_SIZE);
+    memcpy((void*)(dest->username), source + USERNAME_OFFSET, USERNAME_OFFSET);
+    memcpy((void*)(dest->email), source + EMAIL_OFFSET, EMAIL_OFFSET);
+}
+
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t TABLE_MAX_PAGES = 100;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
 typedef struct {
     StatementType type;
+    Row row_to_insert;  // only used by insert statement
 } Statement;
 
 typedef struct {
@@ -82,6 +128,13 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
                                 Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",
+                                   &(statement->row_to_insert.id),
+                                   statement->row_to_insert.username,
+                                   statement->row_to_insert.email);
+        if (args_assigned < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
@@ -91,6 +144,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
 
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
+
 void execute_statement(Statement* statement) {
     switch (statement->type) {
         case (STATEMENT_INSERT):
@@ -101,6 +155,7 @@ void execute_statement(Statement* statement) {
             break;
     }
 }
+
 int main(int argc, char* argv[]) {
     // define a input buffer
     InputBuffer* input_buffer = new_input_buffer();
